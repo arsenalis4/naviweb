e35f9d381787d8ac84d84e0aeca07830f8e26c89
서동재 <hanatoal1234@naver.com>
2023-03-23 16:11:08 +0900

    [Feat] App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [db, setDB] = useState(null);
+  const [userPrice, setUserPrice] = useState(null);
+  const [searchCount, setSearchCount] = useState(0);
+  const priceList = [3000, 4000, 5000, 6000, 7000, 8000, 9000];
+  const onUserPriceChange = useCallback(e => {
+    setUserPrice(e.target.value);
+  });
+
+  useEffect(()=>{
+    if(userPrice === null){
+      //Do Something...
+    } else{
+      axios.post("http://localhost:3000/getStore", {
+        priceInterval: userPrice
+      }).then((res)=>{
+        const data = res.data;
+        const random = Math.floor(Math.random() * data.length);
+        const randomData = data[random];
+
+        const lis = [];   
+        lis.push(<div className='randomRecommend'>오늘은 {randomData.price}원 {randomData.store} {randomData.menu} 어때요?</div>)
+
+        if(searchCount > 0){
+          data.forEach((e)=>{
+            lis.push(<div className='storeBox'><div className='storeBoxMenuInfo'><div className='storeName'>{e.store}</div><div>{e.menu}</div></div><div className='storeBoxPriceInfo'>{e.price} 원</div><div className='storeBoxMapInfo'><a href={e.url}>위치 보러가기</a></div></div>);
+          });
+        }
+        setDB(lis);
+      });
+    }
+  }, [searchCount]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='headerText'>낙성대역 주변 1만원 이하 식당</div>
+      <div className='optionFlex'>
+        <div className='optionItems'>
+          <select className="optionItem" onChange={onUserPriceChange} value={userPrice}>
+            <option value="" selected disabled hidden >가격대를 선택해주세요!</option>
+            {priceList.map((item) => (
+              <option value={item} key={item}>
+                {item}원대
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className='searchButton' onClick={()=>{
+          setSearchCount(searchCount+1);
+        }}>
+          <img src="img/magnifying-glass.png" />
+        </div>
+      </div>
+      <div className='storeInfos'>
+        {db}
+      </div>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const mysql = require('mysql');
 const PORT = process.env.PORT || 3000;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database : 'naviwebtest'
+});
+
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.post('/getStore',function(req,res){
+    var priceInterval= parseInt(req.body.priceInterval);
+    var sql = `SELECT * FROM store WHERE (price >= ${priceInterval}) AND (price < ${priceInterval + 1000})`;
+    con.query(sql, function (err, results, fields) {
+      if (err) throw err;
+      res.send(results);
+      res.end();
+    });
 });
 
   
